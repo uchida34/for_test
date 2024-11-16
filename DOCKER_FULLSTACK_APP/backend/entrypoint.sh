df142bba96899e95f7a61cfacaa75b5eb5e0b839
uchida <ucchiiop@gmail.com>
2024-11-16 14:26:42 +0900

    dbが立ち上がるまで待つように修正

--- DOCKER_FULLSTACK_APP/backend/entrypoint.sh
+++ DOCKER_FULLSTACK_APP/backend/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# MySQL サーバーが起動するまで待機
+while ! mysqladmin ping -h"${DB_HOST}" -P"${DB_PORT}" --silent; do
+    sleep 1
+    echo "Waiting for MySQL to be ready..."
+done
+echo "MySQL is up and running."
+
 # Alembic migrationを実行
 echo "Running Alembic migrations..."
 poetry run alembic upgrade head
